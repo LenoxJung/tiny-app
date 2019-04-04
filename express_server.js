2f78f459a85ddab6c0693f96f3bbcb924be0cabd
Lenox Jung <lenoxjung@gmail.com>
2019-04-04 00:07:19 +0000

    redirects to new tinyurl and link

--- express_server.js
+++ express_server.js
@@ -33,7 +33,7 @@ app.get("/urls", (req, res) => {
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
-})
+});
 
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
@@ -50,7 +50,7 @@ app.post("/urls", (req, res) => {
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
